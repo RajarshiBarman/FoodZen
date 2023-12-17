2cc49f2ee5d105776c6b298ac8ce93a16863dcff
RajarshiBarman <rajarshibarman812@gmail.com>
2023-12-18 00:26:39 +0530

    AWS Modifified

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
+    host:"database-2.ck2hjyid4dt4.us-east-1.rds.amazonaws.com",
+    user:"admin",
     password:"rajarshi",
-    database : "dbms"
+    database : "FoodZen"
 })
 connection.connect((err) => {
     if (err) throw err;
